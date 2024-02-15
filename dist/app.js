a4a033c65ec0fe60c2255e71b32b4f0f12a5a1f8
John Kalberer <jlkalberer@gmail.com>
2024-02-15 10:25:25 -0800

    Redacted token from authorization header.

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -46,7 +57,7 @@ function createApp(container, settings, existingApp) {
         level: 'debug',
         logger: logger,
         logName: 'req_id',
-        obscureHeaders: [],
+        obscureHeaders: ['authorization'],
         propertyName: 'reqId',
     }));
     app.use(function (req, res, next) {
@@ -73,6 +84,10 @@ function createApp(container, settings, existingApp) {
             catch (_) {
                 /* intentionally ignore */
             }
+            var headers = __assign({}, req.headers);
+            if (headers.authorization) {
+                headers.authorization = 'Bearer <removed>';
+            }
             req.log.info({
                 msg: 'Request',
                 url: req.url,
@@ -80,7 +95,7 @@ function createApp(container, settings, existingApp) {
                 bodyRequest: req.body,
                 statusCode: res.statusCode,
                 bodyResponse: bodyResponse,
-                headers: req.headers,
+                headers: headers,
             });
         });
         next();
